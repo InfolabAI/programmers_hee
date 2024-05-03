1abbcedc41976e2ce4357297325038ac81257c04
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-05-03 16:32:13 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,16 @@
 #def solution(brown, yellow):
 #    return
-# 240314
+# 240503
 def solution(brown, yellow):
-    for i in range(1, (yellow+1)//2+1): # NOTE 틀린 부분. (yellow+1)//2 로 하면 안됨.
-        if yellow % i == 0:
-            yw, yh = i, yellow //i
-            if yh * 2 + yw * 2 + 4 == brown:
-                return [yh+2, yw+2]
-    return -1
+    h = 3
+    w = 10000
+    while h <= w:
+        if (brown - h*2) % 2 == 0:
+            w = (brown - h*2) // 2 + 2
+            if yellow == h*w - brown:
+                return [w, h]
+        h += 1
+    return
 
 
 
@@ -26,6 +29,19 @@ def solution(brown, yellow):
 
 
 
+
+
+
+
+
+# 240314
+#def solution(brown, yellow):
+#    for i in range(1, (yellow+1)//2+1): # NOTE 틀린 부분. (yellow+1)//2 로 하면 안됨.
+#        if yellow % i == 0:
+#            yw, yh = i, yellow //i
+#            if yh * 2 + yw * 2 + 4 == brown:
+#                return [yh+2, yw+2]
+#    return -1
 
 
 
