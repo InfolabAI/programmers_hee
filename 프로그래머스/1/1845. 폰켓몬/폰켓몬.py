8b3127687bdacc386c0fa5956a405af3cb275c01
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-05-03 14:41:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.34 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,33 @@
+#def solution(nums):
+#    return
+# 240325
+def solution(ls):
+    return min(len(ls)//2, len(set(ls)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def solution(ls):
+#    return min(len(ls)//2, len(set(ls)))
+
+
+#def solution(nums):
+#    return min(len(set(nums)), len(nums)//2)
+
