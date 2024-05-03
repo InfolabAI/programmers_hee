3e766b33ab0a33c0294a238e0bf161e40d850e27
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-05-03 15:51:30 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,22 +1,21 @@
 #def solution(genres, plays):
 #    return 0
-# 240308 .
 from collections import defaultdict
+# 240503
 def solution(genres, plays):
-    answer = []
-    set_genre = defaultdict(lambda:0)
-    play_id_genre = defaultdict(list)
+    num_for_genres = defaultdict(lambda:0)
+    num_in_genres = defaultdict(list)
     for i, (g, p) in enumerate(zip(genres, plays)):
-        set_genre[g] += p
-        play_id_genre[g] += [(p, i)]
-        
-    sorted_genre_list = sorted(set_genre, key=lambda x:set_genre[x], reverse=True)
-    for g, play_id_list in play_id_genre.items():
-        play_id_genre[g] = sorted(play_id_list, key=lambda x:x[0], reverse=True)
+        num_for_genres[g] += p
+        num_in_genres[g] += [(i, p)]
     
-    for sg in sorted_genre_list:
-        answer += list(map(lambda x:x[1], play_id_genre[sg][:2]))
-        
+    answer = []
+    #print(num_for_genres)
+    #print(num_in_genres)
+    sorted_num_for_genres = sorted(num_for_genres.items(), key=lambda x:x[1], reverse=True)
+    for k, v in sorted_num_for_genres:
+        for i, p in sorted(num_in_genres[k], key=lambda x:x[1], reverse=True)[:2]:
+        	answer += [i]
     return answer
 
 
@@ -27,6 +26,36 @@ def solution(genres, plays):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# 240308 .
+#from collections import defaultdict
+#def solution(genres, plays):
+#    answer = []
+#    set_genre = defaultdict(lambda:0)
+#    play_id_genre = defaultdict(list)
+#    for i, (g, p) in enumerate(zip(genres, plays)):
+#        set_genre[g] += p
+#        play_id_genre[g] += [(p, i)]
+#        
+#    sorted_genre_list = sorted(set_genre, key=lambda x:set_genre[x], reverse=True)
+#    for g, play_id_list in play_id_genre.items():
+#        play_id_genre[g] = sorted(play_id_list, key=lambda x:x[0], reverse=True)
+#    
+#    for sg in sorted_genre_list:
+#        answer += list(map(lambda x:x[1], play_id_genre[sg][:2]))
+#        
+#    return answer
 
 
 
