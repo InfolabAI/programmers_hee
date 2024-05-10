ff1d1d32b5e1b69ec6c45e84c155f890d24c1a11
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-05-10 10:17:46 +0900

    [level 3] Title: 숫자 게임, Time: 40.03 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,15 +1,15 @@
 def solution(A, B):
-    answer = -1
+    return answer
+# 240510
+def solution(A, B):
+    # 예제와 다르게 해결해도 됨. 둘 다 sort 하고 큰 수부터 차례로 비교해도 정답임.
     A.sort(reverse=True)
     B.sort(reverse=True)
-    # NOTE 틀린 부분. 어떤 순서로 구해야 최소의 시간에 문제를 해결하는가? 나이브하게는 product 를 이용해야 하는뎨?
-    # A: 굳이 예제처럼 이기지 않아도 됨. A 큰 수에 대해서 B 를 큰 수부터 차례대로 출전 시키면 그게 최대 승수임.
-    answer = 0 
-    i = 0
+    i, answer = 0,0
     for a in A:
-        if a < B[i]:
-            answer+=1
-            i+=1
+        if B[i] > a:
+            i += 1
+            answer += 1
     
     return answer
 
@@ -32,6 +32,23 @@ def solution(A, B):
 
 
 
+
+
+# 240507
+#def solution(A, B):
+#    answer = -1
+#    A.sort(reverse=True)
+#    B.sort(reverse=True)
+#    # NOTE 틀린 부분. 어떤 순서로 구해야 최소의 시간에 문제를 해결하는가? 나이브하게는 product 를 이용해야 하는뎨?
+#    # A: 굳이 예제처럼 이기지 않아도 됨. A 큰 수에 대해서 B 를 큰 수부터 차례대로 출전 시키면 그게 최대 승수임.
+#    answer = 0 
+#    i = 0
+#    for a in A:
+#        if a < B[i]:
+#            answer+=1
+#            i+=1
+#    
+#    return answer
 
 
 #def solution(A, B):
