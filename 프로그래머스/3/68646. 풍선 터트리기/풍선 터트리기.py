72f53616f1a3406171d527a777eb5cf4da5983f6
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-08-22 09:19:43 +0900

    [level 3] Title: 풍선 터트리기, Time: 969.13 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -2,22 +2,57 @@ def solution(a):
     answer = 0
     return answer
 
-
-
-# ref https://coding-lks.tistory.com/44
+# 240822
+# 최후까지 남은 풍선이 시작점
+# 만약 작은 풍선 터뜨리는 기회가 1번도 없다면, 가작 번호가 작은 풍선만 남게됨
+# NOTE 틀린 부분. 이 공식을 생각하지 못함. 공식: 만약 작은 풍선 터뜨리는 기회가 1번이라면, left 최소값, right 최소값 둘 다 기준 풍선보다 작은게 아니라면 기준 풍선은 남을 수 있음
 def solution(a):
-    answer = 2
+    answer = 0
+    # NOTE 틀린 부분. 정답이지만, 시간초과. minmax 를 사전에 for 문 1번으로 구해놓고 사용해야 함.
+    #for i in range(len(a)):
+    #    left = a[:i] if len(a[:i]) != 0 else [1000000001]
+    #    right = a[i+1:] if len(a[i+1:]) != 0 else [1000000001]
+    #    #print(left, right)
+    #    if a[i] > min(left) and a[i] > min(right):
+    #        continue
+    #    else:
+    #        answer += 1
     
-    if 0 <= len(a) <= 2:
-        return len(a)
+    leftmin, rightmin = a[0], a[-1]
+    minmax = [[0, el, 0] for el in a]
+    for l, r in zip(range(len(a)), reversed(range(len(a)))):
+        # 특정 시점에서 가장 작은 수를 누적 게산
+        leftmin = a[l] if a[l] < leftmin else leftmin
+        rightmin = a[r] if a[r] < rightmin else rightmin
+        minmax[l][0] = leftmin
+        minmax[r][2] = rightmin
+        
+    for l, m, r in minmax:
+        if l < m and r < m:
+            continue
+        else:
+            answer += 1
+        
+    return answer
 
-    left, right = a[0],a[-1]
-    
-    for i in range(1, len(a)-1):
-        if left > a[i]:
-            answer += 1
-            left = a[i]
-        if right > a[-1-i]:
-            answer += 1
-            right = a[-1-i]
-    return answer if left != right else answer-1
+# ref https://tiktaek.tistory.com/m/64
+#def solution(a):
+#    answer = 0
+#    alen = len(a)
+#    left, right = a[0], a[-1]
+#    minmax = [[0, ele, 0] for ele in a]     # 왼쪽 최소, a원소, 오른쪽 최소
+#    for l, r in zip(range(alen), range(len(a) - 1, -1, -1)):
+#        if left > a[l]:
+#            left = a[l]
+#        if right > a[r]:
+#            right = a[r]
+#        minmax[l][0] = left
+#        minmax[r][2] = right
+#
+#    for idx in range(alen):                 # 왼쪽 최소와 오른쪽 최소 중에서
+#        left, mid, right = minmax[idx]      # a의 원소보다 큰수가 하나이하 일때
+#        if left < mid and right < mid:
+#            continue
+#        answer += 1
+#
+#    return answer
