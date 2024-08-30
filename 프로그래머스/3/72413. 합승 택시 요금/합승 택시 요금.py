ff66094483926c2e5ad24bf8dc61760747bdaa14
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-08-30 10:11:59 +0900

    [level 3] Title: 합승 택시 요금, Time: 1195.25 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,28 +1,53 @@
 def solution(n, source, a, b, fares):
     return
-        
-# 240829
+
+# 240830
 def solution(n, source, a, b, fares):
-    answer = n*1000001 # NOTE 틀린 부분. 100001 은 edge 하나당 최고 비용임. 총 최고비용은 모든 node 를 방문하는 비용임.
-    d = [[n*100001 for _ in range(n)] for _ in range(n)]
+    MAX_COST = 100001*n
+    answer = MAX_COST
+    adj = [[MAX_COST for _ in range(n)] for _ in range(n)]
     for i in range(n):
-        d[i][i] = 0
-        
-    for s, t, f in fares:
+        adj[i][i] = 0
+    for s, t, c in fares:
         s, t = s-1, t-1
-        d[s][t] = f
-        d[t][s] = f
-        
-    for k in range(n): # NOTE 틀린 부분. 플로이드-워셜 쓸 생각을 못했음.
+        adj[s][t] = c
+        adj[t][s] = c
+    for k in range(n):
         for s in range(n):
             for t in range(n):
-                tmp = d[s][k] + d[k][t]
-                if tmp < d[s][t]:
-                    d[s][t] = tmp
-    
-    for k in range(n): # NOTE 틀린 부분. 플로이드-워셜 이후, A, B 모두를 최단경로로 이동하는 비용 계산을 하지 못했음.
-        answer = min(answer, d[source-1][k] + d[k][a-1] + d[k][b-1])
+                tmp = adj[s][k] + adj[k][t]
+                if tmp < adj[s][t]:
+                    adj[s][t] = tmp
+                    
+    #for row in adj:
+    #    print(row)
+        
+    for i in range(n):
+        answer = min(answer, adj[source-1][i] + adj[i][a-1] + adj[i][b-1])
     return answer
+        
+# 240829
+#def solution(n, source, a, b, fares):
+#    answer = n*1000001 # NOTE 틀린 부분. 100001 은 edge 하나당 최고 비용임. 총 최고비용은 모든 node 를 방문하는 비용임.
+#    d = [[n*100001 for _ in range(n)] for _ in range(n)]
+#    for i in range(n):
+#        d[i][i] = 0
+#        
+#    for s, t, f in fares:
+#        s, t = s-1, t-1
+#        d[s][t] = f
+#        d[t][s] = f
+#        
+#    for k in range(n): # NOTE 틀린 부분. 플로이드-워셜 쓸 생각을 못했음.
+#        for s in range(n):
+#            for t in range(n):
+#                tmp = d[s][k] + d[k][t]
+#                if tmp < d[s][t]:
+#                    d[s][t] = tmp
+#    
+#    for k in range(n): # NOTE 틀린 부분. 플로이드-워셜 이후, A, B 모두를 최단경로로 이동하는 비용 계산을 하지 못했음.
+#        answer = min(answer, d[source-1][k] + d[k][a-1] + d[k][b-1])
+#    return answer
 
 # ref https://nauco.tistory.com/48 [UNDERSTANDING:티스토리]
 #def solution(n, s, a, b, fares):
