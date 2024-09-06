b6bd5702457c27981e73b73b9bb5768ad3d5f0c1
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-09-06 10:19:53 +0900

    [level 3] Title: 야근 지수, Time: 376.60 ms, Memory: 28.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,15 +1,25 @@
-import numpy as np
-import heapq
-
 def solution(n, works):
-    answer = 0
-    # 즉, n 만큼 works 의 모든 값이 같아지는 방향으로 변경한 후, 야근 지수 계산하면 정답.
-    # n 에 따라, works 의 각 값은 min(works) 보다 작아질 수 있음.
-    heapq.heapify(works := [-w for w in works])
-    for i in range(n):
-        heapq.heappush(works, min(heapq.heappop(works) + 1, 0))
-    return sum([(w)**2 for w in works])
+    return
+
+# 240906
+import numpy as np
+import heapq as hq
+def solution(n, works):
+    for i in range(len(works)): # for maxheap
+        works[i] = -works[i]
         
+    # 큰 수를 제거하면 되므로, max heap 이용
+    hq.heapify(works)
+    for i in range(n):
+        poped = hq.heappop(works)
+        hq.heappush(works, poped + 1)
+        
+    #print(works)
+    answer = 0
+    for i in range(len(works)):
+        answer += works[i]**2 if works[i] < 0 else 0
+    return answer
+    
         
         
         
@@ -58,3 +68,13 @@ def solution(n, works):
 #    for i in range(min(n, abs(sum(works)))):
 #        heappush(works, heappop(works)+1)
 #    return sum([i*i for i in works])
+
+#def solution(n, works):
+#    answer = 0
+#    # 즉, n 만큼 works 의 모든 값이 같아지는 방향으로 변경한 후, 야근 지수 계산하면 정답.
+#    # n 에 따라, works 의 각 값은 min(works) 보다 작아질 수 있음.
+#    heapq.heapify(works := [-w for w in works])
+#    for i in range(n):
+#        heapq.heappush(works, min(heapq.heappop(works) + 1, 0))
+#    return sum([(w)**2 for w in works])
+        
