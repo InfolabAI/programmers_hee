06d1da5d6f091d3ec2c1ce8130799f7b932950ff
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2024-09-10 09:17:14 +0900

    [level 3] Title: 기지국 설치, Time: 4.93 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,33 +1,46 @@
 #def solution(n, stations, w):
 #    return
-# 240510
-import math
 def solution(n, stations, w):
-    prev_s = 0
-    empty_list = []
-    for s in stations:
-        mns, mxs = s-w, s+w
-        empty_list.append(mns-prev_s-1)
-        prev_s = mxs
-        
-    if n+1 - mxs-1 > 0:
-        empty_list.append(n+1 - mxs-1)
+    prev = 0
+    blocks = []
+    stations += [n+w+1]
+    for st in stations:
+        st = st-1
+        tmp = st-w-prev
+        if tmp > 0:
+            blocks.append(tmp)
+        prev = st+w+1
+        #print(st, tmp, prev)
         
     answer = 0
-    for e in empty_list:
-        answer += math.ceil(e/(w*2+1))
-    
+    for bl in blocks:
+        answer += bl // (w*2+1)
+        if bl % (w*2+1) > 0:
+            answer += 1
     return answer
 
 
 
 
 
-
-
-
-
-
+# 240510
+#import math
+#def solution(n, stations, w):
+#    prev_s = 0
+#    empty_list = []
+#    for s in stations: # 빈공간 계산
+#        mns, mxs = s-w, s+w
+#        empty_list.append(mns-prev_s-1)
+#        prev_s = mxs
+#        
+#    if n+1 - mxs-1 > 0: # 끝과의 빈공간 추가
+#        empty_list.append(n+1 - mxs-1)
+#        
+#    answer = 0
+#    for e in empty_list: # 빈 공간마다 필요한 기지국 수 계산
+#        answer += math.ceil(e/(w*2+1))
+#    
+#    return answer
 
 
 
