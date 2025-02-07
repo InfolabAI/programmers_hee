dbc05fdcb8e8d044364e60226667a7a395b6c08e
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2025-02-07 13:14:13 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID
+     , NAME
+  FROM ANIMAL_INS
+ WHERE ANIMAL_TYPE = 'Dog'          -- 개(Dog)만 조회
+   AND NAME IS NOT NULL             -- 이름이 NULL이 아닌 레코드만
+   AND LOWER(NAME) LIKE '%el%'      -- 이름에 "el"이 포함 (대소문자 구분X)
+ ORDER BY NAME;                     -- 이름 순 정렬
