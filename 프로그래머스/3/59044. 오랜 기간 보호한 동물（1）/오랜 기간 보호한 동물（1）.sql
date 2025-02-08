83b03906d27ac6ff20d21257b8ee55e0d83d83a1
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2025-02-08 17:50:21 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,31 @@
--- 코드를 입력하세요
-SELECT AI.NAME
-     , AI.DATETIME
-  FROM ANIMAL_INS AS AI
-  LEFT JOIN ANIMAL_OUTS AS AO 
-         ON AI.ANIMAL_ID = AO.ANIMAL_ID
- WHERE AO.ANIMAL_ID IS NULL           -- (1) 입양 기록이 없는 동물(입양 못 간 동물)
- ORDER BY AI.DATETIME                 -- (2) 가장 오래 보호소에 있던(입소일이 가장 빠른 순)
- LIMIT 3;                             -- (3) 3마리만 조회
+select ai.NAME,	ai.DATETIME
+from animal_ins as ai left join animal_outs as ao on ai.animal_id = ao.animal_id
+where ao.animal_id is null
+order by ai.datetime
+limit 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 250208
+-- -- 코드를 입력하세요
+-- SELECT AI.NAME
+--      , AI.DATETIME
+--   FROM ANIMAL_INS AS AI
+--   LEFT JOIN ANIMAL_OUTS AS AO 
+--          ON AI.ANIMAL_ID = AO.ANIMAL_ID
+--  WHERE AO.ANIMAL_ID IS NULL           -- (1) 입양 기록이 없는 동물(입양 못 간 동물)
+--  ORDER BY AI.DATETIME                 -- (2) 가장 오래 보호소에 있던(입소일이 가장 빠른 순)
+--  LIMIT 3;                             -- (3) 3마리만 조회
