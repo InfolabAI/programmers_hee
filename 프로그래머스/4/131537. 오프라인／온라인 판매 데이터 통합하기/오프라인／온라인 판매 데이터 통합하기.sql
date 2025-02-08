4ba3017caf0d78bebe7311cb6515efd3a6645409
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2025-02-08 17:28:26 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,22 +1,49 @@
-SELECT 
-    DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE, -- DATE 의 포맷 변경
+select DATE_FORMAT(SALES_DATE, '%Y-%m-%d') as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT -- NOTE 틀린 부분. DATE_FORMET 뒤에는 항상 as 로 새 칼럼명 지정해줘야 함
+from ONLINE_SALE
+where SALES_DATE between '2022-03-01' and '2022-03-31'
+
+union
+
+select DATE_FORMAT(SALES_DATE, '%Y-%m-%d') as SALES_DATE, PRODUCT_ID, null as USER_ID, SALES_AMOUNT
+from OFFLINE_SALE
+where SALES_DATE between '2022-03-01' and '2022-03-31'
+
+order by -- NOTE 틀린 부분. 한 번에 order by 하는 법.
+    SALES_DATE,
     PRODUCT_ID,
-    USER_ID,
-    SALES_AMOUNT
-FROM ONLINE_SALE
-WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31' -- 조건 거는 법
+    USER_ID
 
-UNION ALL -- 합치는 법
 
-SELECT 
-    DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE,
-    PRODUCT_ID,
-    NULL AS USER_ID, -- 특정 열을 NULL 로 채우는 법
-    SALES_AMOUNT
-FROM OFFLINE_SALE
-WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
 
-ORDER BY  -- 정렬하는 법
-    SALES_DATE ASC,
-    PRODUCT_ID ASC,
-    USER_ID ASC;
+
+
+
+
+
+
+
+
+
+-- 250208
+-- SELECT 
+--     DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE, -- DATE 의 포맷 변경
+--     PRODUCT_ID,
+--     USER_ID,
+--     SALES_AMOUNT
+-- FROM ONLINE_SALE
+-- WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31' -- 조건 거는 법
+-- 
+-- UNION ALL -- 합치는 법
+-- 
+-- SELECT 
+--     DATE_FORMAT(SALES_DATE,'%Y-%m-%d') AS SALES_DATE,
+--     PRODUCT_ID,
+--     NULL AS USER_ID, -- 특정 열을 NULL 로 채우는 법
+--     SALES_AMOUNT
+-- FROM OFFLINE_SALE
+-- WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+-- 
+-- ORDER BY  -- 정렬하는 법
+--     SALES_DATE ASC,
+--     PRODUCT_ID ASC,
+--     USER_ID ASC;
