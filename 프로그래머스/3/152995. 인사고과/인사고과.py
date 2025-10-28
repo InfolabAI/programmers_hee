2aaaf7164b437c32a75ba58b275b37308ea260ca
Heechul Lim <36654797+InfolabAI@users.noreply.github.com>
2025-10-28 11:22:41 +0900

    [level 3] Title: 인사고과, Time: 168.56 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.py
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -1,32 +1,54 @@
 def solution(scores):
     return
 
-# 240926
-def solution(scores):
-    # 다른 임의의 사원보다 두 점수가 모두 낮은 경우가 한 번이라도 있다면 그 사원은 인센티브를 받지 못함
-    # 인센티브 받는 사람 중 두 점수의 합이 높은 순으로 석차를 냄
-    # 점수 a 로 내림차순 정렬
-    # 점수 b 로 오름차순 정렬
-    st_a, st_b = scores[0]
-    scores = sorted(scores, key=lambda x: (-x[0], x[1]))
-    #print(scores)
+def solution_251028(scores):
     answer = 1
+    ma, mb = scores[0]
+    del scores[0]
+    scores = sorted(scores, key=lambda x:(-x[0], x[1]))
+    maxb = 0 
     
-    maxb = 0
-    for sc in scores:
-        a, b = sc
-        if st_a < a and st_b < b:
+    #print(ma, mb, scores)
+    for i in range(len(scores)):
+        ca, cb = scores[i]
+        if ca > ma and cb > mb:
             return -1
         
-        #if sc[0] + sc[1] > st_a + st_b: # NOTE 틀린 부분. 그 이유는, 높은 점수라도 인센티브 대상이 아니면 등수에 들지 못하기 때문.
-        #    answer += 1
-        
-        if b >= maxb:
-            maxb = b
-            if a+b > st_a+st_b:
+        if cb >= maxb:
+            maxb = cb
+            if ca+cb > ma+mb:
                 answer += 1
-            
+        
+        #print(ca, cb, answer)
+    
     return answer
+
+# 240926
+def solution_240926(scores):
+    # 다른 임의의 사원보다 두 점수가 모두 낮은 경우가 한 번이라도 있다면 그 사원은 인센티브를 받지 못함
+    # 인센티브 받는 사람 중 두 점수의 합이 높은 순으로 석차를 냄
+    # 점수 a 로 내림차순 정렬, 점수 b 로 오름차순 정렬
+    st_a, st_b = scores[0]  # 첫 번째 사원의 a, b 점수를 기준(즉, '나'의 점수)으로 저장
+    scores = sorted(scores, key=lambda x: (-x[0], x[1]))  # a는 내림차순, b는 오름차순으로 정렬
+    #print(scores)
+    answer = 1  # 내 등수를 1등으로 초기화
+
+    maxb = 0  # 현재까지의 b 점수 중 최대값을 저장할 변수 (인센티브 가능한 최대 b)
+    for sc in scores:  # 정렬된 모든 사원을 순회
+        a, b = sc  # 각 사원의 a, b 점수를 분리
+        if st_a < a and st_b < b:  # 만약 나보다 두 점수 모두 높은 사원이 있다면
+            return -1  # 나는 인센티브를 받을 수 없으므로 -1 반환
+
+        # 과거 주석: 단순히 점수 합으로 비교하면 안 됨 (인센티브 미대상자는 등수에 포함되지 않기 때문)(“나보다 점수 합이 높다고 해서 꼭 등수가 높은 것은 아니다.”)
+        #if sc[0] + sc[1] > st_a + st_b:
+        #    answer += 1
+
+        if b >= maxb:  # 현재 사원의 b 점수가 지금까지의 최대 b 이상이면
+            maxb = b  # 최대 b 값 갱신
+            if a + b > st_a + st_b:  # 이 사원의 총합 점수가 나보다 높으면
+                answer += 1  # 등수 +1
+
+    return answer  # 계산된 내 등수 반환
 
 
 
@@ -74,3 +96,8 @@ def solution(scores):
 #                answer += 1
 #            
 #    return answer + 1
+
+
+def solution(scores):
+    #return solution_240926(scores)
+    return solution_251028(scores)
